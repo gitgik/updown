e5ed6ef0c201d9a29c5934e3687abec7e13ae551
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-08-31 18:23:54 +0300

    [feature] update models to use a hashids for naming files

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@
 This file represents the models for the api app.
 """
 from django.db import models
+from .utils import get_file_upload_path, generate_uid
 
 
 class DateMixin(models.Model):
@@ -13,8 +14,8 @@ class DateMixin(models.Model):
 class File(DateMixin):
     """This class represents the file model."""
 
-    name = models.CharField(max_length=100, unique=True)
-    file = models.FileField(allow_files=True)
+    file_id = models.CharField(default=generate_uid, max_length=50)
+    _file = models.FileField(upload_to=get_file_upload_path)
 
     def __str__(self):
         """Return a string representation of the model instance."""
