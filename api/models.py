6f737d6859d41bb1a82e07d0d32d0a2157247def
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-08-31 17:04:23 +0300

    [feature] define the file model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,22 @@
-from __future__ import unicode_literals
-
+"""
+This file represents the models for the api app.
+"""
 from django.db import models
 
-# Create your models here.
+
+class DateMixin(models.Model):
+    """A model mixin for date creation."""
+
+    created = models.DateField(auto_now_add=True)
+
+
+class File(DateMixin):
+    """This class represents the file model."""
+
+    name = models.CharField(max_length=100, unique=True)
+    file = models.FileField(allow_files=True)
+
+    def __str__(self):
+        """Return a string representation of the model instance."""
+        return "{}".format(self.name)
+
