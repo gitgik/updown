02d17303d45356e6c1057bfa30a36bc9361f288a
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-08-31 23:31:50 +0300

    [fix] refactor model tests to test for model file upload

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from api.models import File, DateMixin
 from django.core.files.uploadedfile import SimpleUploadedFile
+import os
 
 
 class ModelsTestCase(TestCase):
@@ -10,12 +11,15 @@ class ModelsTestCase(TestCase):
         """Setup variables."""
         self.file_object = File()
         self.dates = DateMixin()
+        self.test_file_path = os.getcwd() + "/api/tests/test_file.jpeg"
 
     def test_file_creation(self):
-        """Test whether a book can be created."""
+        """Test whether a file can be created using the model."""
+        with open(self.test_file_path, 'rb') as f:
+            jpg_data = f.read()
         self.file_object._file = SimpleUploadedFile(
-            name='test_file.jpeg',
-            content=open('/tests/test_file.jpeg', 'rb').read(),
+            name="test_file.jpeg",
+            content=jpg_data,
             content_type='image/jpeg')
         self.file_object.save()
         self.assertIsInstance(self.file_object, File)
