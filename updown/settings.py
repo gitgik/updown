e9ba39b27fcfc98c134e59cf403b99d61e17c1e9
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-08-31 16:46:27 +0300

    [chore] integrate postgresql to be the default db

--- updown/settings.py
+++ updown/settings.py
@@ -73,10 +73,14 @@ WSGI_APPLICATION = 'updown.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'updown',
+        'HOST': '127.0.0.1',
+        'USER': 'postgres',
+        'PASSWORD': ''
     }
 }
 
