888f32921b468288ca7d1a8455c506687dc179c3
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-08-31 20:45:34 +0300

    [feature] define view set for file uploads

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.viewsets import ModelViewSet
+from .models import File
+from .serializers import FileUploadSerializer
 
-# Create your views here.
+
+class FileUploadViewSet(ModelViewSet):
+    """A viewset to handle file uploads."""
+    queryset = File.objects.all()
+    serializer_class = FileUploadSerializer
+    parser_classes = (FormParser, MultiPartParser)
+
+    def perform_create(self, serializer):
+        serializer.save(self.request.data.get('_file'))
