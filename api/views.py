e641b10cc6b9d9557b3f3d13c7eb3b6c059dd3fc
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-09-02 23:28:43 +0300

    [feature] save a file together with it's associated user

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 from rest_framework.viewsets import ModelViewSet
 from .models import File
 from .serializers import FileUploadSerializer
@@ -10,7 +10,8 @@ class FileUploadViewSet(ModelViewSet):
     """A viewset to handle file uploads."""
     queryset = File.objects.all()
     serializer_class = FileUploadSerializer
-    parser_classes = (FormParser, MultiPartParser)
+    parser_classes = (FormParser, MultiPartParser, JSONParser)
 
     def perform_create(self, serializer):
-        serializer.save(_file=self.request.data.get('_file'))
+        owner = self.request.user
+        serializer.save(owner=owner, _file=self.request.data.get('_file'))
